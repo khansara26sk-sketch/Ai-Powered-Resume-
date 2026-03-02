83745a06379d841c110ff9253c8d56935f5093c1
Sara <khansara26sk@gmail.com>
2026-03-03 04:28:37 +0530

    update changes

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const API_BASE = "";
+const API_BASE =
+  window.location.hostname === "localhost"
+    ? "http://localhost:3000"
+    : "";
 
 /* ============================= */
 /*      FILE PREVIEW LOGIC      */
